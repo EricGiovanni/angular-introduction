ca5153bfc42bea1a5b9c90aa866d06645bf5315f
Eric <giovanny.m.t@hotmail.com>
2019-12-19 13:11:07 -0600

    Primeros servicios firebase

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-grafica-40f4f.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.json({
+      mensaje: 'Hola mundo!!!'
+  });
+});
+
+export const getGOTY = functions.https.onRequest( async(request, response) => {
+
+    const gotyRef = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const juegos =  docsSnap.docs.map(doc => doc.data());
+
+    response.json(juegos);
+
+
+  });
+  
