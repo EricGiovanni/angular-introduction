7a500043f0799320ce5b48752300c0fc82ef772c
Eric <giovanny.m.t@hotmail.com>
2019-12-20 09:57:44 -0600

    Mejoras a la vista

--- goty/src/app/pages/goty/goty.component.ts
+++ goty/src/app/pages/goty/goty.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from 'src/app/services/game.service';
 import { Game } from 'src/app/interfaces/interfaces';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-goty',
@@ -21,6 +22,17 @@ export class GotyComponent implements OnInit {
         this.juegos = juegos;
       });
 
+  }
+
+  votarJuego(juego: Game){
+    this.gameService.votarJuego(juego.id)
+      .subscribe( resp => {
+        
+        if( resp.ok ){
+          Swal.fire('Gracias', resp.mensaje, 'success');
+        }
+
+      });
   }
 
 }
--- goty/src/app/services/game.service.ts
+++ goty/src/app/services/game.service.ts
@@ -2,17 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  juegos: Game[] = [];
+
   constructor( private http: HttpClient ) { }
 
   getNominados(){
 
-    return this.http.get<Game[]>(`${ environment.url }/api/goty`);
-    
+    if(this.juegos.length > 0){
+
+      return of(this.juegos);
+
+    }else{
+      return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+        .pipe(
+          tap(
+            juegos => this.juegos = juegos
+          )
+        );
+    }
+  }
+
+  votarJuego(id: String){
+    return this.http.post<Game[]>(`${ environment.url }/api/goty/${ id }`, {});
   }
 }
