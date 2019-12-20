65e08700478998ed89db8c67195b5aab69686f12
Eric <giovanny.m.t@hotmail.com>
2019-12-19 18:05:42 -0600

    Creacion de interfaz y servicios

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+import * as express from 'express';
+import * as cors from 'cors';
+
 const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -27,6 +30,42 @@ export const getGOTY = functions.https.onRequest( async(request, response) => {
 
     response.json(juegos);
 
-
   });
   
+const app = express();
+
+app.use(cors({ origin: true}));
+
+app.get('/goty', async (req, res) => {
+    const gotyRef = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const juegos =  docsSnap.docs.map(doc => doc.data());
+
+    res.json(juegos);
+
+});
+
+app.post('/goty/:id', async (req, res) => {
+    const id = req.params.id;
+    const gameRef = db.collection('goty').doc(id);
+    const gameSnap = await gameRef.get();
+
+    if(!gameSnap.exists){
+        res.status(404).json({
+            ok: false,
+            mensaje: 'No existe un juego con ese ID ' + id
+        });
+    }else{
+        const antes = gameSnap.data() || { votos: 1 };
+        await gameRef.update({
+            votos: antes.votos + 1
+        });
+        
+        res.json({
+            ok: true,
+            mensaje: `Gracias por tu voto a ${antes.name}`
+        });
+    }
+});
+
+export const api = functions.https.onRequest( app );
